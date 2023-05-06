823ad10d77d90899bf42ccc54b1a710e3c4eff10
Pascal Müller <pamuelle@student.ethz.ch>
2023-05-06 11:14:50 +0200

    moved everything service related outside of api

--- api/api.py
+++ api/api.py
@@ -1,12 +1,18 @@
 from multiprocessing import Process
 import time
-from flask import Flask, Response, send_file, request, make_response
+from flask import Flask, Response, send_file, request, make_response, session
 from flask_cors import CORS
 import os
 from random import random
 from datetime import datetime
 from PIL import Image
 import magic
+
+from service import Service
+
+# TODO: Use secure_filename()
+# TODO: Use sessions
+# TODO: Utilize UPLOAD_FOLDER and MAX_CONTENT_LENGTH and TEMP DIR
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
 CORS(app)
@@ -15,53 +21,6 @@ app.config["UPLOAD_FOLDER"] = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), "static/files"
 )
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
-
-OUTPUT_EXTENSION = ".png"
-
-
-def is_allowed_filetype():
-    # Define allowed mime types
-    allowed_mime_types = ["image/png", "image/jpeg", "text/plain", "application/pdf"]
-
-    # Read the first 2048 bytes from the uploaded file buffer. We don't save it to the
-    # filesystem yet because we don't know wtf it could be.
-    mime = magic.Magic(mime=True)
-    file = request.files["inputFile"]
-    first_2kb = file.read(2048)
-    detected_mimetype = mime.from_buffer(first_2kb)
-
-    # Reset stream to start of file
-    file.stream.seek(0)
-
-    print("\n\n Uploaded mimetype: ", detected_mimetype, "\n\n")
-
-    # Check mimetype
-    if detected_mimetype in allowed_mime_types:
-        return True
-
-    return False
-
-    # return "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_types
-
-
-def get_filename(token, suffix=None):
-    # Get names of all files in the upload folder
-    filenames = os.listdir(app.config["UPLOAD_FOLDER"])
-
-    # Look for the correct filename that contains the token.
-    # Note: "filename" is always unique since we check for uniqueness when we
-    # store it.
-    filename = list(filter(lambda filename: token in filename, filenames))[0]
-
-    extension = filename.split(".")[-1]
-
-    # Get the currently processed file
-    if suffix is not None:
-        final_filename = token + suffix + "." + extension
-    else:
-        final_filename = token + "." + extension
-
-    return final_filename
 
 
 @app.route("/")
@@ -74,63 +33,37 @@ def get_current_time():
     return {"time": time.time()}
 
 
-def something(input):
-    a = 0
-    for i in range(100_000_000):
-        a = i + 1
-    return input
-
-
-def do_something(init_filename):
-    # Load file
-    filepath = os.path.join(app.config["UPLOAD_FOLDER"], init_filename)
-    input = Image.open(filepath)
-
-    # Here we'd change the file somehow. We just wait 3 seconds.
-    time.sleep(3)
-
-    # Update file: We set the input as the output (because we didn't do
-    # anything) and save it undera new name.
-    output = input
-    extension = input.filename.split(".")[-1]
-    out_filename = init_filename.split(".")[0] + "_." + extension
-    save_path = os.path.join(app.config["UPLOAD_FOLDER"], out_filename)
-    output.save(save_path)
-
-
 @app.route("/api/upload", methods=["POST"])
 def upload():
+    # Generate token
+    token = str(int(random() * 10**16))
+
+    # Get file extension
+    file = request.files["inputFile"]
+    input_extension = file.filename.split(".")[-1]
+
+    service = Service(token, input_extension)
+
     # Check if the file contained in the request's body is allowed.
-    if not is_allowed_filetype():
+    if not is_allowed_filetype(service.allowed_mime_types):
         return "Unsupported filetype", 415
 
-    # Generate token
-    file = request.files["inputFile"]
-    extension = file.filename.split(".")[-1]
-    token = int(random() * 10**16)
-
-    # Define Server side filename
-    ss_filename = str(token) + "." + extension
-
-    filepath = os.path.join(app.config["UPLOAD_FOLDER"], ss_filename)
-
     # Store file to filesystem
-    if os.path.isfile(filepath):
+    if os.path.isfile(service.path_input_file):
         # TODO: Maybe just generate a new token?
         return "File with this token already exists.", 409
 
-    file.save(filepath)
+    file.save(service.path_input_file)
 
     # Start service that does something with file.
-    print(os.path.join(app.config["UPLOAD_FOLDER"], ss_filename))
-    p = Process(target=do_something, args=[ss_filename])
+    p = Process(target=service.process)
     p.start()
 
     # Make the response body
     response = {
         "status": f"successfully uploaded file to the server. process still alive: {p.is_alive()}",
         "filename": file.filename,
-        "token": ss_filename.split(".")[0],
+        "token": token,
     }
 
     return response
@@ -140,15 +73,13 @@ def upload():
 def stream():
     token = request.args.get("token")
 
-    final_filename = get_filename(token, suffix="_")
-
-    final_path = os.path.join(app.config["UPLOAD_FOLDER"], final_filename)
+    service = Service(token)
 
     # Wait until the file is processed
     def get_data():
         while True:
             time.sleep(1)
-            if os.path.isfile(final_path):
+            if os.path.isfile(service.path_processed_file):
                 yield f"data: done\n\n"
             else:
                 yield f"data: not done\n\n"
@@ -162,14 +93,29 @@ def download_file():
     data = request.json
     token = data["token"]
 
-    # Get filename
-    ss_filename = get_filename(token, suffix="_")
-
-    filepath = os.path.join(app.config["UPLOAD_FOLDER"], ss_filename)
+    service = Service(token)
 
     # Check if file exists
-    if not os.path.isfile(filepath):
+    if not os.path.isfile(service.path_processed_file):
         return make_response("File not found", 404)
 
     # Send file back to client
-    return send_file(filepath, as_attachment=True)
+    return send_file(service.path_processed_file, as_attachment=True)
+
+
+def is_allowed_filetype(allowed_mime_types):
+    # Read the first 2048 bytes from the uploaded file buffer. We don't save it to the
+    # filesystem yet because we don't know wtf it could be.
+    mime = magic.Magic(mime=True)
+    file = request.files["inputFile"]
+    first_2kb = file.read(2048)
+    detected_mimetype = mime.from_buffer(first_2kb)
+
+    # Reset stream to start of file
+    file.stream.seek(0)
+
+    # Check mimetype
+    if detected_mimetype in allowed_mime_types:
+        return True
+
+    return False
---
+++ api/service.py
@@ -0,0 +1,81 @@
+import os
+import magic
+import time
+from PIL import Image
+
+
+class Service:
+    def __init__(self, token, input_extension=None):
+        # Construct upload folder path
+        self.upload_folder = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "static/files"
+        )
+
+        self.token = token
+        self.suffix = "_processed"
+
+        # self.get_file_extension() assumes the file is already uploaded.
+        # This might not be the case. That's why we can also pass the
+        # input_extension to the constructor.
+        if input_extension == None:
+            self.input_extension = self.get_file_extension()
+        else:
+            self.input_extension = input_extension
+
+        #
+        # SPECIFY THIS BY HAND!
+        #
+        # If we have e.g. images, we can have input as jpg or png and the
+        # output should be the same. That's what we see below.
+        # But if we have e.g. OCR from PDF to txt then input is pdf but output
+        # would be fixed to .txt
+        self.output_extension = self.input_extension
+
+        #
+        # SPECIFY THIS BY HAND!
+        #
+        # Define allowed mime types
+        self.allowed_mime_types = [
+            "image/png",
+            "image/jpeg",
+            "text/plain",
+            "application/pdf",
+        ]
+
+        self.path_input_file = os.path.join(
+            self.upload_folder, self.token + "." + self.output_extension
+        )
+        self.path_processed_file = os.path.join(
+            self.upload_folder, self.token + self.suffix + "." + self.output_extension
+        )
+
+    def get_file_extension(self):
+        # Get names of all files in the upload folder
+        filenames = os.listdir(self.upload_folder)
+
+        # Look for the correct filename that contains the token.
+        # Note: "filename" is always unique since we check for uniqueness when we
+        # store it.
+        filename = list(filter(lambda filename: self.token in filename, filenames))[0]
+
+        extension = filename.split(".")[-1]
+
+        return extension
+
+    def process(self, **kwargs):
+        # Load file
+        input = Image.open(self.path_input_file)
+
+        # Here we'd change the file somehow. We just wait 3 seconds.
+        time.sleep(3)
+
+        # Update file: We set the input as the output (because we didn't do
+        # anything) and save it undera new name.
+        output = input
+        output.save(self.path_processed_file)
+
+    def something(input):
+        a = 0
+        for i in range(100_000_000):
+            a = i + 1
+        return input
